8d00db572b6f9a5864461fa487d167378e2ce30e
RaquelD-B <154616671+RaquelD-B@users.noreply.github.com>
2024-04-26 08:53:36 -0400

    Added js functionality for Paris and Tokyo Time

--- src/index.js
+++ src/index.js
@@ -6,6 +6,21 @@ function updateTime() {
   losAngelesTimeElement.innerHTML = `${losAngelesTime.format(
     "h:mm:ss [<small>]A[</small>]"
   )}`;
+  let parisDateElement = document.querySelector("#paris-date");
+  let parisTime = moment.tz("Europe/Paris");
+  let parisTimeElement = document.querySelector("#paris-time");
+  parisDateElement.innerHTML = parisTime.format("MMMM Do, YYYY");
+  parisTimeElement.innerHTML = `${parisTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  )}`;
+
+  let tokyoDateElement = document.querySelector("#tokyo-date");
+  let tokyoTime = moment.tz("Asia/Tokyo");
+  let tokyoTimeElement = document.querySelector("#tokyo-time");
+  tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do, YYYY");
+  tokyoTimeElement.innerHTML = `${tokyoTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  )}`;
 }
 updateTime();
 setInterval(updateTime, 1000);
